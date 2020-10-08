769d8ec50e6fc1bb56cc04683154bbd452127811
MINKYONGTAE <gt_min@naver.com>
2020-10-08 18:28:25 +0900

    updated 2020-10-08

--- javastudy/workspace/12_exception/src/ex02_exception_class/Ex01_AgeException.java
+++ javastudy/workspace/12_exception/src/ex02_exception_class/Ex01_AgeException.java
@@ -1,5 +1,7 @@
 package ex02_exception_class;
 
+import java.util.Scanner;
+
 // 이미 만들어진 예외클래스 : Exception 상속 받는다.
 // 개발자가 만드는 예외클래스 : Exception 상속 받는다.
 
@@ -42,12 +44,51 @@ class Person {
 
 	// 매개변수 int age로 전달되는 값에 따라서,
 	// 예외가 발생할 수 있는 메소드
-	public void setAge(int age) {
+	public void setAge(int age) throws AgeException {
+		if (age < 0 || age > 150) {
+			AgeException e = new AgeException("나이는 0~150만 가능합니다.");
+			throw e;  // 개발자가 만든 예외는 직접 던진다.
+		}
 		this.age = age;
 	}
 	
 }
 
+// 나이가 0 ~ 150 범위가 아니면
+// 생성할 예외클래스
+class AgeException extends Exception {
+	// constructor
+	public AgeException(String message) {
+		super(message);
+	}
+}
+
+public class Ex01_AgeException {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			
+			Person p = new Person("홍길동", 20);
+			
+			// 나이 수정
+			System.out.print("나이를 입력하세요 >>> ");
+			int age = sc.nextInt();
+			p.setAge(age);
+			System.out.println("변경된 나이: " + p.getAge());
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			
+		} finally {
+			sc.close();
+		}
+		
+	}
+	
+}
 	/*
 	 * 메소드가 예외를 처리하는 방법
 	 * 1. 직접 try - catch 문을 작성한다. (지금 고려할 대상이 아님)
@@ -93,11 +134,3 @@ class Person {
 	*/
 	////////////////////////////////////////////
 
-public class Ex01_AgeException {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
