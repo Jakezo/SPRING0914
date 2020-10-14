cedea03bd6017d8e5c609d691c28c520f36a4cb1
MINKYONGTAE <gt_min@naver.com>
2020-10-14 12:48:34 +0900

    updated 2020-10-14

--- javastudy/workspace/13_api/src/ex04_quiz/Quiz04.java
+++ javastudy/workspace/13_api/src/ex04_quiz/Quiz04.java
@@ -1,6 +1,43 @@
 package ex04_quiz;
 
-
+class Score {
+	// field
+	private int kor;
+	private int eng;
+	private int mat;
+	// constructor
+	public Score(int kor, int eng, int mat) {
+		super();
+		this.kor = kor;
+		this.eng = eng;
+		this.mat = mat;
+	}
+	// method
+	@Override
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("국어: ").append(kor);
+		sb.append(", 영어: ").append(eng);
+		sb.append(", 수학: ").append(mat);
+		return sb.toString();
+	}
+	// score1.equals(score2)
+	@Override
+	public boolean equals(Object obj) {
+		// this : score1
+		// obj  : score2
+		if (this == obj) {  // score1.equals(score1)
+			return true;
+		}
+		if (obj instanceof Score) {
+			Score score = (Score) obj;
+			// return this.kor == score.kor && this.eng == score.eng && this.mat == score.mat;
+			return kor == score.kor && eng == score.eng && mat == score.mat;
+		}
+		return false;  // obj가 Score가 아님
+	}
+	
+}
 
 public class Quiz04 {
 
@@ -9,6 +46,7 @@ public class Quiz04 {
 		Score score1 = new Score(80, 90, 100);
 		Score score2 = new Score(80, 90, 100);
 		
+		// toString() 사용
 		System.out.println(score1);  // 국어: 80, 영어: 90, 수학: 100
 		System.out.println(score2);  // 국어: 80, 영어: 90, 수학: 100
 		
@@ -21,12 +59,3 @@ public class Quiz04 {
 	}
 
 }
-
-
-
-
-
-
-
-
-
