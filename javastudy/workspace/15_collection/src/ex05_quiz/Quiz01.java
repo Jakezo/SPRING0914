7a8b0696da09c1a5ce8e840721e9a77e4566f359
MINKYONGTAE <gt_min@naver.com>
2020-10-15 14:21:08 +0900

    updated 2020-10-15

---
+++ javastudy/workspace/15_collection/src/ex05_quiz/Quiz01.java
@@ -0,0 +1,24 @@
+package ex05_quiz;
+
+import java.util.Date;
+
+// 문제.
+// 임의의 User를 3명 저장하는 ArrayList를 생성하시오.
+// 회원번호를 입력 받아 검색하여 일치하는 회원을 삭제하시오.
+
+class User {
+	// field
+	private int no;     // 회원번호
+	private String id;  // 아이디
+	private Date date;  // 가입일 (현재 날짜)
+	
+}
+
+public class Quiz01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
