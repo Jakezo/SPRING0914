f3aeaef00aef544272757a812019b4d454505343
MINKYONGTAE <gt_min@naver.com>
2020-10-16 14:52:29 +0900

    updated 2020-10-16

---
+++ javastudy/workspace/16_io/src/ex01_byte_base/Ex09_ObjectOutputStream.java
@@ -0,0 +1,30 @@
+package ex01_byte_base;
+
+import java.io.Serializable;
+
+/*
+ * ObjectOutputStream 클래스
+ * 1. 객체(object)를 파일에 출력하는 클래스이다.
+ * 2. 객체를 스트림으로 입출력 처리하려면 해당 객체를 직렬화해야 한다.
+ * 3. 직렬화하려면 Serializable 인터페이스를 구현하면 된다.
+ */
+
+class User implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3296197331300114753L;
+
+	
+}
+
+public class Ex09_ObjectOutputStream {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
