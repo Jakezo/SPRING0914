ecdfb308da4cadb2ec1b5f7dc6a59a9ae691cc61
MINKYONGTAE <gt_min@naver.com>
2020-10-22 11:33:25 +0900

    updated 2020-10-22

---
+++ dbstudy/11. 기본 SQL 작성하기_DDL 활용_실습.sql
@@ -0,0 +1,10 @@
+-- 실습
+-- 회원 테이블: MEMBERS
+/*
+    1. 회원번호: NO, NUMBER, 기본키
+    2. 회원아이디: ID, VARCHAR2(30), 필수, 중복 불가
+    3. 회원패스워드: PW, VARCHAR2(30), 필수
+    4. 회원포인트: POINT, NUMBER
+    5. 회원등급: GRADE, VARCHAR2(10), 'VIP', 'GOLD', 'SILVER', 'BRONZE' 중 하나
+    6. 회원이메일: EMAIL, VARCHAR2(100), 중복 불가
+*/
