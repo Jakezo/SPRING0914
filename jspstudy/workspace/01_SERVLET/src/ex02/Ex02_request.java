f2479c7ba977f72aa6ab20905493fc9d3ff13a71
MINKYONGTAE <gt_min@naver.com>
2020-12-01 18:38:16 +0900

    updated 2020-12-01

---
+++ jspstudy/workspace/01_SERVLET/src/ex02/Ex02_request.java
@@ -0,0 +1,24 @@
+package ex02;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Ex02_request")
+public class Ex02_request extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    public Ex02_request() {
+        super();
+    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String query = request.getParameter("query");
+		System.out.println(query);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
