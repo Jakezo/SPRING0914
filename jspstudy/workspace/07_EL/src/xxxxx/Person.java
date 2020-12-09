ee2682f6077bf7d5d60206a76493078fee615a5f
MINKYONGTAE <gt_min@naver.com>
2020-12-09 12:48:12 +0900

    updated 2020-12-09

---
+++ jspstudy/workspace/07_EL/src/xxxxx/Person.java
@@ -0,0 +1,28 @@
+package xxxxx;
+
+public class Person {
+
+	private String name;
+	private int age;
+	private char gender;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+	public String getName() { 
+		return name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public char getGender() {
+		return gender;
+	}
+	
+}
