b31760cfb020f69993609d4ed2e6198806622c05
MINKYONGTAE <gt_min@naver.com>
2020-12-21 16:29:25 +0900

    updated 2020-12-21

--- jspstudy/workspace/17_AJAX/src/command/BlackJSONCommand.java
+++ jspstudy/workspace/17_AJAX/src/command/BlackJSONCommand.java
@@ -1,15 +1,23 @@
 package command;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+
+import dao.BlackDao;
+import dto.BlackDto;
 
 public class BlackJSONCommand implements BlackCommand {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) {
 		
+		/* 동작 확인용 */
+		/*
 		JSONObject obj = new JSONObject();  // simple-json-1.1.1.jar
 		obj.put("name", "제시카");
 		obj.put("age", 20);
@@ -17,6 +25,25 @@ public class BlackJSONCommand implements BlackCommand {
 		obj.put("phone", "010-9999-9999");
 		obj.put("address", "속초");
 		return obj.toJSONString();
+		*/
+		
+		List<BlackDto> list = BlackDao.getInstance().list();
+		
+		// list를 JSON으로 만들기
+		
+		// JSONObject를 BlackDto로 보면 되기 때문에
+		// list는 JSONArray로 보면 된다.
+		JSONArray jsonArray = new JSONArray();
+		for (BlackDto blackDto : list) {
+			JSONObject obj = new JSONObject();
+			obj.put("name", blackDto.getName());
+			obj.put("age", blackDto.getAge());
+			obj.put("gender", blackDto.getGender());
+			obj.put("phone", blackDto.getPhone());
+			obj.put("address", blackDto.getAddress());
+			jsonArray.add(obj);  // JSONArray는 ArrayList처럼 사용한다.
+		}
+		return jsonArray.toJSONString();
 		
 	}
 
---
+++ jspstudy/workspace/18_CAPTCHA/src/command/GetImageCaptchaCommand.java
@@ -0,0 +1,189 @@
+package command;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import common.PathNRedirect;
+
+public class GetImageCaptchaCommand implements Command {
+
+	@Override
+	public PathNRedirect execute(HttpServletRequest request, HttpServletResponse response) {
+		
+		// 네이버 캡차 API
+		
+		
+		// 1) 캡차 키 발급 요청하기
+		// 2) 캡차 이미지 요청하기
+        String clientId = "u7aGTrFmp005OR4CJWy4"; //애플리케이션 클라이언트 아이디값";
+        String clientSecret = "V00UJonEUP"; //애플리케이션 클라이언트 시크릿값";
+		
+        
+        // 1) 캡차 키 발급 요청하기
+        String code = "0"; // 키 발급시 0,  캡차 이미지 비교시 1로 세팅
+        String apiURL = "https://openapi.naver.com/v1/captcha/nkey?code=" + code;
+
+        Map<String, String> requestHeaders = new HashMap<>();
+        requestHeaders.put("X-Naver-Client-Id", clientId);
+        requestHeaders.put("X-Naver-Client-Secret", clientSecret);
+        String responseBody = get(apiURL, requestHeaders);
+        // System.out.println(responseBody);  // {"key":"bgbl5MwSZRnQOllo"}
+		// responseBody는 {"key":"bgbl5MwSZRnQOllo"}와 같은 형식의 JSON 데이터
+        
+        // json-simple-1.1.1.jar를 이용해서 responseBody에서 "bgbl5MwSZRnQOllo"를 뺀다.
+        JSONParser parser = new JSONParser();
+        JSONObject obj = null;
+        try {
+        	obj = (JSONObject)parser.parse(responseBody);
+        } catch (ParseException e) {
+        	e.printStackTrace();
+        }
+        
+        // 2) 캡차 이미지 요청하기
+        String key = (String)obj.get("key"); // https://openapi.naver.com/v1/captcha/nkey 호출로 받은 키값
+        String apiURL2 = "https://openapi.naver.com/v1/captcha/ncaptcha.bin?key=" + key;
+
+        // requestHeaders는 1) 캡차 키 발급 요청에서 이미 생성했으므로 또 생성할 필요가 없다.
+        /*
+        Map<String, String> requestHeaders = new HashMap<>();
+        requestHeaders.put("X-Naver-Client-Id", clientId);
+        requestHeaders.put("X-Naver-Client-Secret", clientSecret);
+        */
+        String responseBody2 = get2(apiURL2, requestHeaders);
+
+        System.out.println(responseBody2);
+        
+		return null;
+		
+	}
+	
+	// 1) 캡차 키 발급 요청용 get() 메소드
+    private static String get(String apiUrl, Map<String, String> requestHeaders){
+        HttpURLConnection con = connect(apiUrl);
+        try {
+            con.setRequestMethod("GET");
+            for(Map.Entry<String, String> header :requestHeaders.entrySet()) {
+                con.setRequestProperty(header.getKey(), header.getValue());
+            }
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 호출
+                return readBody(con.getInputStream());
+            } else { // 에러 발생
+                return readBody(con.getErrorStream());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("API 요청과 응답 실패", e);
+        } finally {
+            con.disconnect();
+        }
+    }
+    
+	// 1) 캡차 키 발급 요청용 connect() 메소드
+    // 2) 캡차 이미지 요청용 connect() 메소드
+    private static HttpURLConnection connect(String apiUrl){
+        try {
+            URL url = new URL(apiUrl);
+            return (HttpURLConnection)url.openConnection();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("API URL이 잘못되었습니다. : " + apiUrl, e);
+        } catch (IOException e) {
+            throw new RuntimeException("연결이 실패했습니다. : " + apiUrl, e);
+        }
+    }
+
+    // 1) 캡차 키 발급 요청용 readBody() 메소드
+    private static String readBody(InputStream body){
+        InputStreamReader streamReader = new InputStreamReader(body);
+
+        try (BufferedReader lineReader = new BufferedReader(streamReader)) {
+            StringBuilder responseBody = new StringBuilder();
+
+            String line;
+            while ((line = lineReader.readLine()) != null) {
+                responseBody.append(line);
+            }
+
+            return responseBody.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("API 응답을 읽는데 실패했습니다.", e);
+        }
+    }
+    
+    // 2) 캡차 이미지 요청용 get2() 메소드
+    private static String get2(String apiUrl, Map<String, String> requestHeaders){
+    	HttpURLConnection con = connect(apiUrl);
+    	try {
+    		con.setRequestMethod("GET");
+    		for(Map.Entry<String, String> header :requestHeaders.entrySet()) {
+    			con.setRequestProperty(header.getKey(), header.getValue());
+    		}
+    		
+    		int responseCode = con.getResponseCode();
+    		if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 호출
+    			return getImage(con.getInputStream());
+    		} else { // 에러 발생
+    			return error(con.getErrorStream());
+    		}
+    	} catch (IOException e) {
+    		throw new RuntimeException("API 요청과 응답 실패", e);
+    	} finally {
+    		con.disconnect();
+    	}
+    }
+    
+    // 2) 캡차 이미지 요청용 getImage() 메소드
+    private static String getImage(InputStream is){
+        int read;
+        byte[] bytes = new byte[1024];
+        // 랜덤한 이름으로  파일 생성
+        String filename = Long.valueOf(new Date().getTime()).toString();
+        File f = new File(filename + ".jpg");
+        try(OutputStream outputStream = new FileOutputStream(f)){
+            f.createNewFile();
+            while ((read = is.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, read);
+            }
+            return "이미지 캡차가 생성되었습니다.";
+        } catch (IOException e) {
+            throw new RuntimeException("이미지 캡차 파일 생성에 실패 했습니다.",e);
+        }
+    }
+
+    // 2) 캡차 이미지 요청용 error() 메소드
+    private static String error(InputStream body) {
+        InputStreamReader streamReader = new InputStreamReader(body);
+
+        try (BufferedReader lineReader = new BufferedReader(streamReader)) {
+            StringBuilder responseBody = new StringBuilder();
+
+            String line;
+            while ((line = lineReader.readLine()) != null) {
+                responseBody.append(line);
+            }
+
+            return responseBody.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("API 응답을 읽는데 실패했습니다.", e);
+        }
+    }
+
+}
