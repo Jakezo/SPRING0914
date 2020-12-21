d64d960c4ab5e055bb40720cfa36b61f86f09753
MINKYONGTAE <gt_min@naver.com>
2020-12-21 12:49:19 +0900

    updated 2020-12-21

--- jspstudy/workspace/17_AJAX/src/dao/BlackDao.java
+++ jspstudy/workspace/17_AJAX/src/dao/BlackDao.java
@@ -1,7 +1,11 @@
 package dao;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
+import dto.BlackDto;
 import mybatis.config.DBService;
 
 public class BlackDao {
@@ -23,7 +27,12 @@ public class BlackDao {
 	
 	
 	// DB처리 메소드
-	
+	public List<BlackDto> list() {
+		SqlSession ss = factory.openSession();
+		List<BlackDto> list = ss.selectList("mybatis.mapper.black.blackList");
+		ss.close();
+		return list;
+	}
 	
 	
 	
