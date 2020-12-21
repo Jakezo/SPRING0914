b5080ae949b2129f6eb74049835f6f20922f2491
MINKYONGTAE <gt_min@naver.com>
2020-12-21 12:06:29 +0900

    updated 2020-12-21

--- jspstudy/workspace/17_AJAX/src/command/BlackCommand.java
+++ jspstudy/workspace/17_AJAX/src/command/BlackCommand.java
@@ -7,6 +7,13 @@ import common.PathNRedirect;
 
 public interface BlackCommand {
 
-	public PathNRedirect execute(HttpServletRequest request, HttpServletResponse response);
+	/* 일반 Command */
+	// public PathNRedirect execute(HttpServletRequest request, HttpServletResponse response);
+	
+	/* AJAX Command */
+	// 요청한 JSP로 값을 반환하므로 path, redirect/forward가 필요 없다.
+	// 반환 값이 실제 데이터이다.
+	// 모든 반환 값은 String 타입으로 반환한다.
+	public String execute(HttpServletRequest request, HttpServletResponse response);
 	
 }
--- jspstudy/workspace/17_AJAX/src/controller/BlackController.java
+++ jspstudy/workspace/17_AJAX/src/controller/BlackController.java
@@ -1,6 +1,8 @@
 package controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import command.BlackCommand;
+import command.BlackStringCommand;
 import common.PathNRedirect;
 
 @WebServlet("*.black")
@@ -27,14 +30,25 @@ public class BlackController extends HttpServlet {
 		String contextPath = request.getContextPath();
 		String cmd = requestURI.substring(contextPath.length());
 		
-		PathNRedirect pathNRedirect = null;
+		/* AJAX를 처리하는 Constroller는 PathNRedirect가 필요하지 않다. */
+		// PathNRedirect pathNRedirect = null;
+		
+		/* AJAX는 실제 데이터를 String 타입으로 반환하므로 String result가 필요하다. */
+		String result = null;
 		
 		BlackCommand command = null;
 		
 		switch (cmd) {
 		
+		case "/getString.black":
+			command = new BlackStringCommand();
+			result = command.execute(request, response);
+			break;
+		
 		}
 		
+		/* AJAX를 처리하는 Constroller는 PathNRedirect가 필요하지 않다. */
+		/*
 		String path = pathNRedirect.getPath();
 		boolean isRedirect = pathNRedirect.isRedirect();
 		
@@ -43,6 +57,12 @@ public class BlackController extends HttpServlet {
 		} else {
 			request.getRequestDispatcher(path).forward(request, response);
 		}
+		*/
+		
+		// 반환 값(결과)을 요청한 곳으로 보내면 된다.
+		// 응답
+		PrintWriter out = response.getWriter();
+		out.println(result);  // AJAX 통신이 성공하면 success: function(data) { ... }의 data로 result가 전달된다.
     	
 	}
     
