693c6d1261d4e9bc955af1bc24ba92e8eeb3f3b2
MINKYONGTAE <gt_min@naver.com>
2021-01-05 10:48:39 +0900

    updated 2021-01-05

--- jspstudy/workspace/MyHome/src/command/board/ReplyInsertCommand2.java
+++ jspstudy/workspace/MyHome/src/command/board/ReplyInsertCommand2.java
@@ -7,7 +7,7 @@ import common.PathNRedirect;
 import dao.BoardDao;
 import dto.BoardDto;
 
-public class ReplyInsertCommand implements BoardCommand {
+public class ReplyInsertCommand2 implements BoardCommand {
 
 	@Override
 	public PathNRedirect execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
@@ -20,27 +20,37 @@ public class ReplyInsertCommand implements BoardCommand {
 		int bNo = Integer.parseInt(request.getParameter("bNo"));
 		String bIp = request.getRemoteAddr();
 		
-		// 댓글이 달릴 게시글(원글)의 정보 가져오기
+		/*
+		댓글 삽입 시
+		1. bGroup: 게시글 bNo와 같다.
+		2. bGroupOrd
+		   1) 1을 가진다.
+		   2) 기존 댓글들의 bGroupOrd를 1씩 증가시킨다. (별도의 db작업)
+		3. bDepth: 게시글의 bDepth + 1이다.
+		*/
+		
+		// 댓글이 달릴 게시글의 정보 가져오기
 		BoardDto boardDto = BoardDao.getInstance().boardView(bNo);
 		
-		// 1. 같은 bGroup에서 원글의 gGroupOrd보다 큰 bGroupOrd를 1씩 증가시킨다.
-		
-		
-		// 2. 댓글 만들기
-		//    1) bGroup		: 원글의 bGroup (boardDto.getbGroup())
-		//    2) bGroupOrd	: 원글의 bGroupOrd + 1 (boardDto.getbGroupOrd() + 1)
-		//    3) bDepth		: 원글의 bDepth + 1
+		// 댓글 만들기
 		BoardDto replyDto = new BoardDto();
 		replyDto.setbTitle(bTitle);
 		replyDto.setbContent(bContent);
 		replyDto.setmId(mId);
 		replyDto.setbIp(bIp);
-		replyDto.setbGroup(boardDto.getbGroup());
-		replyDto.setbGroupOrd(boardDto.getbGroupOrd() + 1);
+		replyDto.setbGroup(bNo);  // replyDto.setbGroup(boardDto.getbNo());
+		replyDto.setbGroupOrd(1);
 		replyDto.setbDepth(boardDto.getbDepth() + 1);
 		
 		// 댓글 삽입
 		int result = BoardDao.getInstance().replyInsert(replyDto);
+		
+		// 기존에 달린 댓글들의 bGroupOrd를 모두 1씩 증가시킨다.
+		// (가장 먼저 달린 댓글의 bGroupOrd값이 가장 크다.)
+		// 기존에 달린 댓글: bGroup이 같고, bGroupOrd가 0이 아닌(bDepth가 0이 아닌) 게시글
+		if (result > 0) {
+			BoardDao.getInstance().replyUpdatebGroupOrd(bNo);
+		}
 		
 		PathNRedirect pathNRedirect = new PathNRedirect();
 		pathNRedirect.setPath("board/replyInsertResult.jsp?result=" + result + "&page=" + page);
