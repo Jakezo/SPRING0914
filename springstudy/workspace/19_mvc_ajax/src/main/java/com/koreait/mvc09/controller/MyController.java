39bac473a8d77ab80adc3b934ba44a6a96c71cda
MINKYONGTAE <gt_min@naver.com>
2021-01-12 16:48:37 +0900

    updated 2021-01-12

--- springstudy/workspace/19_mvc_ajax/src/main/java/com/koreait/mvc09/controller/MyController.java
+++ springstudy/workspace/19_mvc_ajax/src/main/java/com/koreait/mvc09/controller/MyController.java
@@ -3,16 +3,35 @@ package com.koreait.mvc09.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MyController {
 
+	
 	@RequestMapping(value="/",
 					method=RequestMethod.GET)
 	public String index() {
 		return "index";
 	}
 	
+	
+	@RequestMapping(value="getText",
+					method=RequestMethod.GET,
+					produces="text/plain; charset=utf-8")  // return text; 할 때  text가 "text/plain" 타입입니다.
+	
+	@ResponseBody  // return 하는 데이터는 응답입니다. (뷰나 다른 매핑값이 아니고, 데이터를 반환합니다.)
+	public String getText(@RequestParam(value="send") String send) {
+		
+		String text = send + "를 받았습니다.";
+		
+		// ajax는 반환하는(return) 값이 "뷰"도 아니고, "다른 매핑값"도 아닙니다.
+		// 실제로 데이터(텍스트, json, xml 등)를 반환합니다.
+		
+		return text;  // text는 데이터를 의미합니다. return 하는 데이터는 요청한 곳으로 보내는 데이터입니다. 즉 응답(response)하는 방식입니다.
+		
+	}
 	
 	
 	
--- springstudy/workspace/19_mvc_ajax/src/main/webapp/WEB-INF/views/index.jsp
+++ springstudy/workspace/19_mvc_ajax/src/main/webapp/WEB-INF/views/index.jsp
@@ -10,6 +10,28 @@
 
 <script type="text/javascript">
 	
+	// 페이지 로드 이벤트
+	$(function(){
+		fn_send1();
+	});
+	
+	function fn_send1() {
+		$('#btn1').click(function(){
+			$.ajax({
+				url: 'getText',  // RequestMapping의 value를 작성합니다.
+				type: 'get',  // RequestMapping의 method를 작성합니다.
+				data: 'send=' + $('#send').val(),  // controller로 보내는 값(파라미터)
+				dataType: 'text',  // controller에게서 받아 오는 값의 타입
+				success: function(responseText) {  // responseText: controller에게서 받아 오는 값
+					$('#content1').text(responseText.trim());  // trim(): 불필요한 공백 제거
+				},
+				error: function(){
+					alert('실패');
+				}
+			});
+		});
+	}
+	
 </script>
 
 
@@ -17,7 +39,18 @@
 </head>
 <body>
 
+	<form>
 	
+		보내는 값<br/>
+		<input type="text" id="send" name="send" /><br/><br/>
+		
+		<input type="button" id="btn1" value="일반텍스트" /><br/>
+		<div id="content1"></div>
+	
+	</form>
 
 </body>
 </html>
+
+
+
