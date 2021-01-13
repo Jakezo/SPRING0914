2018f6e1da09043b3e9674d4287ac5c6646a12b6
MINKYONGTAE <gt_min@naver.com>
2021-01-13 11:56:11 +0900

    updated 2021-01-13

--- springstudy/workspace/20_quiz/src/main/java/com/koreait/mvc10/controller/MyController.java
+++ springstudy/workspace/20_quiz/src/main/java/com/koreait/mvc10/controller/MyController.java
@@ -1,6 +1,7 @@
 package com.koreait.mvc10.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,7 +16,11 @@ public class MyController {
 	}
 	
 	@RequestMapping(value="calc", method=RequestMethod.GET)
-	public String calc(Calculator calculator) {
+	public String calc(Calculator calculator, Model model) {
+		
+		// 뷰(JSP)로 전달할 데이터를 model에 저장합니다.
+		// 저장할 때 속성(attribute)으로 저장합니다. (파라미터가 아닙니다.)
+		model.addAttribute("calculator", calculator);
 		
 		calculator.setResult();  // 연산이 수행됩니다.
 		if (calculator.getViewName().equals("result")) {
