b6ea4fb0e240d1ff4387e275d0a7da9b4244bf47
MINKYONGTAE <gt_min@naver.com>
2021-01-18 11:46:31 +0900

    updated 2021-01-18

--- springstudy/workspace/27_Mybatis_overview/src/main/java/com/koreait/mybatis1/controller/UserController.java
+++ springstudy/workspace/27_Mybatis_overview/src/main/java/com/koreait/mybatis1/controller/UserController.java
@@ -1,12 +1,52 @@
 package com.koreait.mybatis1.controller;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.koreait.mybatis1.dao.UsersDao;
 
 @Controller
 public class UserController {
 
 	// Mybatis를 사용하는 경우에는
 	// SqlSession 클래스를 사용합니다.
+	
+	// field
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	
+	// method
+	
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public String index() {
+		return "index";
+	}
+	
+	@RequestMapping(value="usersInsertPage.do", method=RequestMethod.GET)
+	public String usersInsertPage() {
+		return "users/usersInsertPage";
+	}
+	
+	
+	/*
+		UsersDao와 users.xml이 연결되어 있습니다.
+		UsersDao의 특정 메소드를 호출하면 연결된 users.xml의 쿼리가 실행됩니다.
+	*/
+	@RequestMapping(value="usersListPage.do", method=RequestMethod.GET)
+	public String usersListPage(Model model) {
+		UsersDao usersDao = sqlSession.getMapper(UsersDao.class);
+		model.addAttribute("list", usersDao.usersList());
+		return "users/usersListPage";
+	}
+	
+	
+	
 	
 	
 	
