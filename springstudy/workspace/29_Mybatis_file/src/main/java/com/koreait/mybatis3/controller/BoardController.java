35cedd8bcc851d9f8f6c5051796833d29a0a8fef
MINKYONGTAE <gt_min@naver.com>
2021-01-22 12:36:18 +0900

    updated 2021-01-22

--- springstudy/workspace/29_Mybatis_file/src/main/java/com/koreait/mybatis3/controller/BoardController.java
+++ springstudy/workspace/29_Mybatis_file/src/main/java/com/koreait/mybatis3/controller/BoardController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import com.koreait.mybatis3.command.BoardInsertCommand;
 import com.koreait.mybatis3.command.BoardListCommand;
 import com.koreait.mybatis3.command.BoardViewCommand;
 import com.koreait.mybatis3.command.DownloadCommand;
+import com.koreait.mybatis3.command.EmailAuthCommand;
 
 @Controller
 public class BoardController {
@@ -23,28 +25,27 @@ public class BoardController {
 	@Autowired
 	private SqlSession sqlSession;
 	
-	
 	private BoardListCommand boardListCommand;
 	private BoardInsertCommand boardInsertCommand;
 	private BoardViewCommand boardViewCommand;
 	private BoardDeleteCommand boardDeleteCommand;
 	private DownloadCommand downloadCommand;
+	private EmailAuthCommand emailAuthCommand;
 	
 	@Autowired
 	public void setCommand(BoardListCommand boardListCommand,
 			               BoardInsertCommand boardInsertCommand,
 			               BoardViewCommand boardViewCommand,
 			               BoardDeleteCommand boardDeleteCommand,
-			               DownloadCommand downloadCommand) {
+			               DownloadCommand downloadCommand,
+			               EmailAuthCommand emailAuthCommand) {
 		this.boardListCommand = boardListCommand;
 		this.boardInsertCommand = boardInsertCommand;
 		this.boardViewCommand = boardViewCommand;
 		this.boardDeleteCommand = boardDeleteCommand;
 		this.downloadCommand = downloadCommand;
+		this.emailAuthCommand = emailAuthCommand;
 	}
-	
-	
-	
 	
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String index() {
@@ -99,6 +100,24 @@ public class BoardController {
 		model.addAttribute("request", request);
 		boardDeleteCommand.execute(sqlSession, model);
 		return "redirect:boardListPage.do";
+		
+	}
+	
+	
+	/***** 이메일 인증 *****/
+	
+	@Autowired
+	private JavaMailSender mailSender;
+	
+	@RequestMapping(value="emailAuth.do", method=RequestMethod.POST)
+	public String emailAuth(HttpServletRequest request,
+			                Model model) {
+		
+		model.addAttribute("request", request);
+		model.addAttribute("mailSender", mailSender);
+		emailAuthCommand.execute(sqlSession, model);
+		
+		return "board/emailAuthConfirm";
 		
 	}
 	
